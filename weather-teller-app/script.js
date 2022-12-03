76c2b6164782a6a5a7031c5c6a42732e2f6b2d9a
Narmin Afandizada <nafandizada12323@ada.edu.az>
2022-12-03 23:21:52 +0400

    Update script.js

---
+++ weather-teller-app/script.js
@@ -0,0 +1,9 @@
+const main = document.querySelector(".main"),
+inputPart = document.querySelector(".input_sec"),
+infoTxt = inputPart.querySelector(".info"),
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button"),
+weatherPart = main.querySelector(".weather_sec"),
+wIcon = weatherPart.querySelector("img"),
+arrowBack = main.querySelector("header i");
+let api;
